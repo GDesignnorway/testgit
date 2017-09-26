842093cb9ac2eef764cde5034e53de43fe4835c7
Joakim <31492779+GDesignnorway@users.noreply.github.com>
2017-09-26 19:28:09 +0200

    ravenous

--- ravenous/src/App.js
+++ ravenous/src/App.js
@@ -26,12 +26,17 @@ const businesses = [
 ];
 
 
+
+
 class App extends Component {
+  searchYelp(term, location, best_match) {
+    return 'Searching Yelp with Pizza, Brooklyn, {this.best_match}';
+  }
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
-        <SearchBar />
+        <SearchBar searchYelp={this.searchYelp}/>
         <BusinessList businesses={businesses}/>
       </div>
     );
